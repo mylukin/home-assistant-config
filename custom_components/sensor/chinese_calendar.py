eed36522ddc0f5357fbd6b40021771f049a79d76
Lukin <luxiansheng@mogoroom.com>
2018-02-16 22:06:42 +0800

    增加中国法定假日
    commit for 2018-02-16 22:06:34
    RM custom_components/sensor/anyone_there.py -> custom_components/sensor/chinese_calendar.py
     M sensor/date_time.yaml

--- custom_components/sensor/chinese_calendar.py
+++ custom_components/sensor/chinese_calendar.py
@@ -1,28 +1,27 @@
 import logging
-import RPi.GPIO as GPIO
 from datetime import timedelta
+from datetime import datetime
 
 import voluptuous as vol
-import requests
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (
-    TEMP_CELSIUS, CONF_MONITORED_CONDITIONS, CONF_NAME, STATE_UNKNOWN,
-    ATTR_ATTRIBUTION)
+from homeassistant.const import (CONF_NAME, STATE_UNKNOWN, ATTR_DATE, ATTR_ATTRIBUTION)
 from homeassistant.helpers.entity import Entity
 
-REQUIREMENTS = ['RPi.GPIO==0.6.3']
+import chinese_calendar as calendar
+
+REQUIREMENTS = ['chinesecalendar==1.0.3']
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "anyone_there"
+DOMAIN = "chinese_calendar"
 
-CONF_ATTRIBUTION = "是否有人"
+CONF_ATTRIBUTION = "中国节假日"
 
-DEFAULT_NAME = 'Anyone There'
+DEFAULT_NAME = 'Chinese Calendar'
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=1)
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string
@@ -32,22 +31,17 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def setup_platform(hass, config, add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
 
-    add_devices([AnyoneThereSensor(name)], True)
+    add_devices([ChineseCalendarSensor(name)], True)
 
 
-class AnyoneThereSensor(Entity):
+class ChineseCalendarSensor(Entity):
     """Implementation of the Yahoo! weather sensor."""
 
     def __init__(self, name):
         """Initialize the sensor."""
         self._name = name
         self._state = STATE_UNKNOWN
-
-        GPIO.setmode(GPIO.BCM)
-
-        self.PIR_PIN = 21
-
-        GPIO.setup(self.PIR_PIN, GPIO.IN)
+        self._attr_name = ''
 
     @property
     def name(self):
@@ -60,6 +54,17 @@ class AnyoneThereSensor(Entity):
         return self._state
 
     @property
+    def state_attributes(self):
+        """Return the state attributes.
+
+        Implemented by component base class.
+        """
+        return {
+            ATTR_DATE: datetime.today().date().strftime('%Y-%m-%d'),
+            'name': self._attr_name
+        }
+
+    @property
     def device_state_attributes(self):
         """Return the state attributes."""
         return {
@@ -68,8 +73,9 @@ class AnyoneThereSensor(Entity):
 
     def update(self):
         """Get the latest data from Yahoo! and updates the states."""
-
-        if GPIO.input(self.PIR_PIN):
-            self._state = 'yes'
+        is_holiday, holiday_name = calendar.get_holiday_detail()
+        if is_holiday:
+            self._state = 'holiday'
+            self._attr_name = holiday_name
         else:
-            self._state = 'no'
+            self._state = 'workday'
