646534d89c1ef481c4600e336208d502386e5670
Lukin <luxiansheng@mogoroom.com>
2018-02-20 12:36:09 +0800

    commit for 2018-02-20 12:36:09  M custom_components/alexa_intent/intent.py

--- custom_components/alexa_intent/intent.py
+++ custom_components/alexa_intent/intent.py
@@ -707,7 +707,7 @@ class IntentResponse:
         """Set directives response."""
         self.directives[directive_type] = {
             'audio_type': audio_type,
-            'audio_url': audio_url,
+            'audio_url': audio_url.render(),
         }
 
     @callback
