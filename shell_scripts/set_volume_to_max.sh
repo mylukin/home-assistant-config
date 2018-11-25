1d01d4f82628f48840d85cc24662b0b3f8aae807
Lukin <luxiansheng@mogoroom.com>
2018-11-25 22:10:58 +0800

    commit for 2018-11-25 22:10:58  M automation/cuckoo_clock.yaml  M automation/hass_start.yaml  M automation/notify_luvis.yaml  M automation/notify_weather.yaml  M automation/sunrise.yaml  M automation/sunset.yaml R  automation/welcome_home.yaml -> automation/welcome_home.disabled  M shell_scripts/set_volume_to_max.sh

--- shell_scripts/set_volume_to_max.sh
+++ shell_scripts/set_volume_to_max.sh
@@ -5,5 +5,5 @@ cd `dirname $cwd`
 if [ -f .shairport-status ] && [ $(cat .shairport-status) == 'playing' ]; then
     echo "set_volume_to_max: "$(date +'%Y-%m-%d %X') >> /tmp/shairport-status.log
 else
-    amixer -c 0 sset 'PCM',0 90% unmute
+    amixer -c 0 sset 'PCM',0 100% unmute
 fi
